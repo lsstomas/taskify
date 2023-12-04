f391b4489de1b0a67679785b8dcd695d55b39bf2
Luis <luisftomasprado@gmail.com>
2023-12-04 00:17:46 -0300

    Customizações finais

--- taskify/settings.py
+++ taskify/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +156,5 @@ EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = config("EMAIL_USE_TLS")
 
 # --------------------------------------------------------------------------#
+
+django_heroku.settings(locals())
